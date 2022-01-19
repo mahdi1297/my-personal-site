a8b3f81ced1f6048913fd21a1e2b256a5610d3f7
mahdi <mahdi9193514418@gmail.com>
2022-01-19 04:11:52 +0330

    add cors functionality to api

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import cluster from "cluster";
 import helmet from "helmet";
-import cors from "cors";
 import totalCPUs from "os";
 import fileUpload from "express-fileupload";
 import cookieParser from "cookie-parser";
@@ -9,9 +8,9 @@ import BaseRoutes from "./BaseRoutes";
 import path from "path";
 import morgan from "morgan";
 import * as dotenv from "dotenv";
+const cors = require("cors");
 
 const app = express();
-const route = express.Router();
 
 dotenv.config({ path: __dirname + "/.env" });
 dotenv.config();
@@ -32,13 +31,20 @@ if (cluster.isPrimary) {
         cluster.fork();
     });
 } else {
-    app.use(express.json({ limit: "50mb" }))
+    app.use(
+        cors({
+            origin: ["http://localhost:3006", "http://localhost:4200"],
+            optionsSuccessStatus: 200,
+        })
+    )
+        .use(express.json({ limit: "50mb" }))
         .use(express.urlencoded({ limit: "50mb" }))
         .use(fileUpload())
-        .use(cors())
         .use(helmet())
         .use(cookieParser())
         .use(morgan("dev"));
+
+    const route = express.Router();
 
     route.use(function (req, res, next) {
         res.setHeader("Access-Control-Allow-Origin", "*");
