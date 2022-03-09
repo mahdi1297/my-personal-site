9577da4b62699e62153f6fff1efc99625f51c083
mahdi1297 <57314641+mahdi1297@users.noreply.github.com>
2022-03-09 04:36:21 +0330

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,6 +6,4 @@ export const environment = {
   production: false,
   api_url: 'http://localhost:5000/api/v1/',
   api_image_url: 'http://localhost:5000/',
-  JWT_SECRET_KEY:
-    'SKLZDSDSFG_8437??????843Y5dkjslkdfjsdflksdjfsdlkfjsdlfjzlhjdfd!@#$)(*^&*^92384YSDJKVFH*&^(*&#@(*&@(SDFHSOKDFHAOSDIFHSDAOASDFHSUFDISHDFAISDUFG)))',
 };
