638a97575a82343db2c3e276851818c58de43f7e
mahdi1297 <57314641+mahdi1297@users.noreply.github.com>
2022-06-05 12:25:39 +0430

    envioroments deleted

--- frontend/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-  production: false,
-  api_url: 'http://localhost:5000/api/v1/',
-  api_image_url: 'http://localhost:5000/',
-};
